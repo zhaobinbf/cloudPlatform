dee32489b47d5723414692e7d525bac24809e8d5
zhaobinbf@163.com <zhaobinbf@163.com>
2016-11-01 00:23:59 +0800

    test

---
+++ configServer/src/main/java/com/funny/cloud/config/Application.java
@@ -0,0 +1,30 @@
+package com.funny.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhaobin-pc on 2016/10/31.
+ */
+@Configuration
+@EnableAutoConfiguration
+@RestController
+@EnableConfigServer
+public class Application {
+
+
+
+    
+    @RequestMapping("/sayHello")
+    public String sayHello(){
+        return "test";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class);
+    }
+}
